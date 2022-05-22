fa4d2486f98c95280680797bc11817dfabb31a35
ghaithm20 <92829880+ghaithm20@users.noreply.github.com>
2022-05-22 08:52:50 -0700

    this is a save test

--- sumbmit 5.php
+++ sumbmit 5.php
@@ -4,7 +4,7 @@ if((isset($_GET["d1"]))&&isset($_GET["d2"])){
     $d1=$_GET["d1"];
     $d2=$_GET["d2"];
     if(($d1=="u1")&&($d2=="p1")){
-        echo "1";
+        echo "3";
     }
     else {
         echo "0";
